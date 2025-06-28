71690c2145318464afc7de6231794fcb3bcff234
Manikandan Velusamy <mani.app92@gmail.com>
2025-06-28 20:04:00 +0530

    Changed initial val to first array

--- FindLargestNumber.js
+++ FindLargestNumber.js
@@ -1,5 +1,5 @@
 function findLargestNumber(arr){
-    let tempLargestNumber = -Infinity;
+    let tempLargestNumber = arr[0];
 
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > tempLargestNumber){
